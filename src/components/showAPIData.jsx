09388a6f6ac112ba2d10adea51d5b21d0cf8d712
Javier Díaz <jajajavier79@gmail.com>
2021-11-01 22:42:14 -0400

    Fixing API problems

--- src/components/showAPIData.jsx
+++ src/components/showAPIData.jsx
@@ -5,12 +5,19 @@ const baseURL = "http://34.227.3.72:4000/api/public/result_award/get_last_result
 
 export default function ShowAPIData(){
   const [draw, setDraw] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
+    axios.get(`${baseURL}/asdf`).then((response) => {
       setDraw(response.data);
+    }).catch(error=>{
+      setError(error);
     });
   }, []);
+
+  if (error) return `Error: ${error.message}`;
+  if(!draw) return "Error trying to get date"
+
   console.log(draw)
     return(
       <h1>a</h1>
