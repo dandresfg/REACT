b3b7ce5fdab4f0f712f34e1d7bae2cad76b2a203
Javier Díaz <jajajavier79@gmail.com>
2021-11-01 22:25:22 -0400

    Fixing API problems

--- src/components/showAPIData.jsx
+++ src/components/showAPIData.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-const baseURL = "http://34.227.3.72:4000/api/public/result_award/get_last_result";
+const baseURL = "http://34.227.3.72:4000/api/public/result_award/get_last_result?date=2021-04-07";
 
 export default function ShowAPIData(){
   const [draw, setDraw] = React.useState(null);
@@ -11,11 +11,8 @@ export default function ShowAPIData(){
       setDraw(response.data);
     });
   }, []);
-    
+  console.log(draw)
     return(
-      <div>
-        <h1>{draw.draws.id}</h1>
-        <p>{draw.prev_draws[0]}</p>
-      </div>
+      <h1>a</h1>
     ); 
 }
