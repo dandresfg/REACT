a5014f5c7bfd53f1e6b498a7b8f6bd68b43e6c54
Javier Díaz <jajajavier79@gmail.com>
2021-11-03 12:17:21 -0400

    Axios-test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Navigation from './components/navigation.jsx'
 import SortBanner from './components/banner.jsx'
-import DrawsList from './components/draws.jsx'
 
 function App() {
   return(
-    <><Navigation /><SortBanner /><DrawsList /></>
+    <><Navigation /><SortBanner /></>
   );
 }
 
